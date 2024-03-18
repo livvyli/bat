7e991d71d86bd33a9158ef327b2ad984b015f300
AinZolceffli <124689978+AinZolceffli@users.noreply.github.com>
2024-03-18 15:42:38 +0000

    Update mdm 4.m

--- mdm.m
+++ mdm.m
@@ -1,3 +1,119 @@
+%the latest until 112
+%find total length of the data
+data = readtable('trajectory_21.txt');
+sizeOfData = size(data);
+N = sizeOfData(1);
+
+
+% define t and tau
+w = 2;
+det_t = 0.02;
+t_min = w * det_t;
+t_max = (N - w)* det_t;
+t = t_min: 0.02 : t_max;
+
+% tau_min = -t_min; 
+% tau_max = N * det_t;
+% %tau = tau_min: 0.02 :tau_max;
+% tau=-extime:0.02:tau_max
+
+%define vi and vj
+time = table2array(data(:, 1));
+ax = table2array(data(:, 2));
+ay = table2array(data(:, 3));
+bx = table2array(data(:, 4));
+by = table2array(data(:, 5));
+%difference between consecutive position measurements
+delta_ax = diff(ax);
+delta_ay = diff(ay);
+delta_bx = diff(bx);
+delta_by = diff(by);
+delta_time = diff(time);
+%velocity of a and b
+vx_a = delta_ax ./ delta_time;
+vy_a = delta_ay ./ delta_time;
+vx_b = delta_bx ./ delta_time;
+vy_b = delta_by ./ delta_time;
+
+v_mag_a = sqrt(vx_a.^2 + vy_a.^2);
+v_mag_b = sqrt(vx_b.^2 + vy_b.^2);
+%unit vector for a and b
+v_unit_ax = vx_a ./ v_mag_a;
+v_unit_ay = vy_a ./ v_mag_a;
+v_unit_bx = vx_b ./ v_mag_b;
+v_unit_by = vy_b ./ v_mag_b;
+
+% Create lookup tables for velocity components and magnitudes
+vx_a_lookup = containers.Map(time(1:end-1), vx_a);
+vy_a_lookup = containers.Map(time(1:end-1), vy_a);
+vx_b_lookup = containers.Map(time(1:end-1), vx_b);
+vy_b_lookup = containers.Map(time(1:end-1), vy_b);
+v_mag_a_lookup = containers.Map(time(1:end-1), v_mag_a);
+v_mag_b_lookup = containers.Map(time(1:end-1), v_mag_b);
+
+
+
+% define the dot product
+total_tau = (-(N - w) * det_t):0.02:(N * det_t);
+tau_max = N * det_t;
+rownames=t_min: 0.02 : t_max;
+columnnames=-t_max:0.02:tau_max;
+% Convert row and column names to cell arrays of strings
+rownames_cell = arrayfun(@num2str, rownames, 'UniformOutput', false);
+columnnames_cell = arrayfun(@num2str, columnnames, 'UniformOutput', false);
+
+% Convert combined_matrix to a table
+T = array2table(NaN(length(rownames),length(columnnames)), 'VariableNames', columnnames_cell, 'RowNames', rownames_cell);
+
+
+
+for idx_t = 1:length(t)
+    tau_min = -(t(idx_t));
+    tau_max = N * det_t;
+    tau = tau_min:0.02:tau_max;
+
+    for idx_tau = 1:length(tau)
+        %sum_vij = 0;
+        %valid_count = 2*w+1;
+        % Look up velocity components and magnitudes based on time
+        % Calculate row index based on t(idx_t)
+        row_idx = find(rownames == t(idx_t));
+        
+        % Calculate column index based on tau(idx_tau)
+        col_idx = find(columnnames == tau(idx_tau));
+        if isKey(vx_a_lookup, t(idx_t)) && isKey(vx_b_lookup, t(idx_t) + tau(idx_tau))
+            % Get the components of the vector at time idx_t
+            v1_x = vx_a_lookup(t(idx_t));
+            v1_y = vy_a_lookup(t(idx_t));
+            % Normalize the vector
+            v1_mag = sqrt(v1_x^2 + v1_y^2);
+            v1_x_normalized = v1_x / v1_mag;
+            v1_y_normalized = v1_y / v1_mag;
+            
+            % Get the components of the vector at time idx_t + idx_tau
+            v2_x = vx_b_lookup(t(idx_t) + tau(idx_tau));
+            v2_y = vy_b_lookup(t(idx_t) + tau(idx_tau));
+            % Normalize the vector
+            v2_mag = sqrt(v2_x^2 + v2_y^2);
+            v2_x_normalized = v2_x / v2_mag;
+            v2_y_normalized = v2_y / v2_mag;
+            
+            % Calculate dot product
+            v_ab = v1_x_normalized * v2_x_normalized + v1_y_normalized * v2_y_normalized;
+
+            
+            % Store v_ab in dot_products at the correct index
+            T{rownames_cell(row_idx), columnnames_cell(col_idx)} = v_ab;
+        else
+            % If one of the keys is missing, store NaN
+             T{rownames_cell(row_idx), columnnames_cell(col_idx)} = v_ab;
+           
+        end
+    end
+end
+
+
+
 %the latest start at 412
 %ihad continue what u are doing. but i dont delete yours, mine until row 138
 %find total length of the data
